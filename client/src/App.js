496dfd2df95538060256d23736fc23506e7f1fc4
Rom Ezer <romezer@gmail.com>
2020-02-02 00:50:01 +0200

    frontend changed

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             <br />
             <Link to="/otherpage">Other Page</Link>
           </header>
+          <h3>Hi!</h3>
           <div>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
