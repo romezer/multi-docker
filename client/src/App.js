04faac751e4b17f375eb42d8c967c22ae299bda9
Rom Ezer <romezer@gmail.com>
2020-04-08 13:31:08 +0300

    Chnage client h1

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Fib Calaulator</h1>
             <Link to="/">Home</Link>
             <br />
             <Link to="/otherpage">Other Page</Link>
